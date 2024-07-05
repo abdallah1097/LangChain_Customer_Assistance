75013db5cdbee8dccb9e637ac1abc55946bc4da1
Abdallah WallyAllah <a.wallyallah@esbaar.com>
2024-07-05 18:05:39 +0400

    Remove FastAPI app to use UI

--- app.py
+++ app.py
@@ -1,18 +1,13 @@
-from fastapi import Body, FastAPI
-
 from src.customer_assistance_Agent import CustomerAssistanceAgent
 
-app = FastAPI()
-
-
-@app.post("/answer")
-async def answer(question: str = Body(...)):
-    """
-    Receives a question in the request body and returns the answer from the pipeline.
-    """
-    pass
-
-
-# Run the main APP
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8888)
+    # Define CustomerAssistanceAgent instance
+    agent = CustomerAssistanceAgent()
+
+    question = "What is LLRT used for?"
+
+
+    answer = agent.pipeline.run(question)
+    response = agent.prompt.format(question=question, answer=answer)
+
+    print(response)
