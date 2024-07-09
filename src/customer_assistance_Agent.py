7701bcf732ecf7a11782c39e266ad9914fe71360
Abdallah WallyAllah <a.wallyallah@esbaar.com>
2024-07-09 22:04:20 +0400

    Change splitting scheme

--- src/customer_assistance_Agent.py
+++ src/customer_assistance_Agent.py
@@ -5,7 +5,7 @@ os.environ["HUGGINGFACEHUB_API_TOKEN"] = "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"
 # logging.captureWarnings(True) # Ignore all warnings
 
 # Langchain libraries for building the retrieval-based QA pipeline
-from langchain_community.document_loaders import TextLoader  # Load documents from text files
+from langchain_community.document_loaders.generic import GenericLoader  # Load documents from text files
 from langchain_community.embeddings import HuggingFaceEmbeddings  # Generate embeddings for documents using Hugging Face models
 from sentence_transformers import SentenceTransformer
 from langchain_community.llms import HuggingFaceHub # Access pre-trained models from Hugging Face Hub
@@ -25,9 +25,7 @@ class CustomerAssistanceAgent():
         self.repo_id = "openai-community/gpt2" # "distilbert/distilgpt2" # "facebook/opt-125m" # "openai-community/gpt2" # "google/flan-t5-large"
         self.model_kwargs = {"temperature": 0.01, "max_new_tokens": 250, "top_k": 1, "repetition_penalty":1.03}
         self.embedding_model_kargs = {}
-        self.data_path = './data/menu.txt'
-        self.chunk_size = 500
-        self.chunk_overlap = 50
+        self.data_path = './data'
         self.db_path = "faiss_index"
 
         # Initialize Models
@@ -65,20 +63,21 @@ class CustomerAssistanceAgent():
         """
         Loads text dataset from attribute self.data_path and return splitted docs
         """
-        loader = TextLoader(self.data_path)
-        documents = loader.load()
+        # Create a GenericLoader instance specifying the directory and file extension
+        loader = GenericLoader.from_filesystem(path=self.data_path, suffixes=[".txt"])
+
+        # This loads all the .txt files in a lazy manner (loads them on demand)
+        documents = loader.lazy_load()
 
         # Split the documents into smaller chunks (optional)
-        text_splitter = CharacterTextSplitter(chunk_size=self.chunk_size, chunk_overlap=self.chunk_overlap)
+        text_splitter = CharacterTextSplitter()
         splitted_document = text_splitter.split_documents(documents)
 
-        docs = [doc.page_content for doc in splitted_document]
-
         # See the splitted text document
-        print(f"\n[INFO] Data Document Splitted into: {len(docs)} Chuncks {len(docs[0])} Char Each!")
-        for i, doc in enumerate(docs, 0):
-            print(f"    Chunck [{i}]: {len(doc)} Char | Starts with:\n        {doc[:50]}")
-        return docs
+        print(f"\n[INFO] Data Document Splitted into: {len(splitted_document)} Chuncks {len(splitted_document[0].page_content)} Char Each!")
+        for i, doc in enumerate(splitted_document, 0):
+            print(f"    Chunck [{i}]: {len(doc.page_content)} Char | Starts with:\n        {doc.page_content}")
+        return splitted_document
 
     def create_faiss_db(self, docs):
         db = FAISS.from_documents(docs, self.embeddings_model)
